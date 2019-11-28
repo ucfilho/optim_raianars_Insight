d922e6415098fcef0156eecb48b0b7f43d1253c8
ucfilho <42356381+ucfilho@users.noreply.github.com>
2019-11-28 20:57:40 -0300

    Create AvaliaX.py

---
+++ AvaliaX.py
@@ -0,0 +1,18 @@
+import Fun
+
+def AvaliaX(X):
+  
+  nrow,ncol=X.shape
+  fitness = np.asarray([Fun(ind) for ind in X])
+  best_idx = np.argmin(fitness)
+  best = X[best_idx]
+  fobj_best = fitness[best_idx]
+  y=fitness
+  BEST=best
+  FOBEST=fobj_best
+  XY= np.c_[X,y] #concatena x e y em 2 colunas            
+  XYsorted = XY[XY[:,-1].argsort()] #Ordena a partir da last col(Y) for all row
+  x=XYsorted[:,0:ncol]
+  XY=XYsorted
+  BEST_XY =np.append(BEST,FOBEST)
+  return XY,BEST_XY,BEST,FOBEST
